4faf88399f98cd3b66e6630a1d9b851da4b5be4d
Elbekjon <96689605+ilmnajot@users.noreply.github.com>
2024-10-13 23:19:51 +0500

    Hotel_Management

--- src/main/java/uz/ilmnajot/revolution_task/security/JwtProvider.java
+++ src/main/java/uz/ilmnajot/revolution_task/security/JwtProvider.java
@@ -1,19 +1,13 @@
 package uz.ilmnajot.revolution_task.security;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import uz.ilmnajot.revolution_task.exception.ForbiddenException;
-
 import javax.crypto.SecretKey;
-import java.security.Key;
 import java.util.Date;
 import java.util.function.Function;
 
